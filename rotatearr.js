bc8644abc6b6a1d65b897effe55c2d9d14369f48
rajm688 <90443069+rajm688@users.noreply.github.com>
2021-11-28 15:49:19 +0530

    Add files via upload

---
+++ rotatearr.js
@@ -0,0 +1,21 @@
+const rotateArray = function(nums, k) {
+
+    // reverse helper function
+    function reverse(arr, start, end) {
+      while (start < end) {
+        [arr[start], arr[end]] = [arr[end], arr[start]];
+        start++;
+        end--;
+      }
+    }
+  
+    k %= nums.length;
+  
+    reverse(nums, 0, (nums.length - 1));
+    reverse(nums, 0, (k - 1));
+    reverse(nums, k, (nums.length - 1));
+  
+    return nums;
+  }
+  let arr1 =rotateArray([1,2,3,4,5],2)
+  console.log(arr1);
